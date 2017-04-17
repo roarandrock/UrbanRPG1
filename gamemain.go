b0f5eaf21c8b3b278238536abe2cd7a29ad30bd5
roarandrock <robertwuest@Roberts-MacBook-Air.local>
2017-04-17 22:51:35 +0200

    Starting file

---
+++ gamemain.go
@@ -0,0 +1,18 @@
+/*
+Author: roarandrock
+Company: Jupiter Engineering
+
+Motto: "Better to produce shit than to be constipated"
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("Game start")
+	log.Println(log.Ldate)
+}
